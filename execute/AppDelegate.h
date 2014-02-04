15d7a1f4624abd6c8afc659cbf871c7601e335e5
Ricardo Vazquez <rvazquez@freshbooks.com>
2014-02-04 08:51:25 -0500

    first commit

---
+++ execute/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  execute
+//
+//  Created by Ricardo Vazquez on 2014-02-04.
+//  Copyright (c) 2014 Ricardo Vazquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ executeTests/executeTests.m
@@ -0,0 +1,34 @@
+//
+//  executeTests.m
+//  executeTests
+//
+//  Created by Ricardo Vazquez on 2014-02-04.
+//  Copyright (c) 2014 Ricardo Vazquez. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface executeTests : XCTestCase
+
+@end
+
+@implementation executeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
